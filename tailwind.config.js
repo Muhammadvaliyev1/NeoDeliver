5773a99f8bcf13fd0dcb5baa29c2b4a3a8093a3d
Muhammadvaliyev1 <127742778+Muhammadvaliyev1@users.noreply.github.com>
2024-02-10 19:43:01 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Inter: ['Inter'],
+      },
+      colors: {
+        'primary': '#E6EFFE',
+        'primary-200': '#74A2ED',
+        'primary-600': '#1539E7',
+        'primary-700': '#1D46F3',
+        'grayDefoult': '#6B7280',
+        'gray-60': '#667085',
+        'gray-40': '#E5E7EB',
+        'gray-1000': '#6B7280',
+        'yello': '#FFBA12',
+        'cyan': '#F2F7FE'
+      },
+      maxWidth: {
+        '8xl': '1440px',
+      }
+    },
+  },
+  plugins: [],
+}
+
