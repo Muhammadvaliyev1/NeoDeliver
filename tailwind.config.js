6c438cba33ff99756f086d7e8d483d66220ca9b6
Muhammadvaliyev1 <127742778+Muhammadvaliyev1@users.noreply.github.com>
2024-02-21 15:54:07 +0500

    added new page

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,14 @@ module.exports = {
       colors: {
         'primary': '#E6EFFE',
         'primary-200': '#74A2ED',
+        'primary-300': '#F1F7FD',
         'primary-600': '#1539E7',
         'primary-700': '#1D46F3',
         'grayDefoult': '#6B7280',
-        'gray-60': '#667085',
+        'gray-10': '#F9FAFB',
         'gray-40': '#E5E7EB',
+        'gray-60': '#667085',
+        'gray-90': '#1D2939',
         'gray-1000': '#6B7280',
         'yello': '#FFBA12',
         'cyan': '#F2F7FE'
